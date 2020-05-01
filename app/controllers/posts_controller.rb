e752de9d9f005d38b394467b6d11f889be694c8d
jasonkim7288 <jason.kim7288@gmail.com>
2020-05-01 15:56:02 +1000

    post search added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     if params[:search]
-      @posts = Post.search(params[:search]).all.order('created_at DESC').paginate(per_page: 3, page: params[:page])
+      @posts = Post.search(params[:search]).order('created_at DESC').paginate(per_page: 3, page: params[:page])
     else
       @posts = Post.all.order("created_at DESC").paginate(per_page: 3, page: params[:page])
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,12 @@ class Post < ApplicationRecord
 
     has_one_attached :image
     has_rich_text :body
+    has_one :action_text_rich_text,
+        class_name: "ActionText::RichText",
+        as: :record
 
     def self.search(query)
-        where("title like ? OR body like ?", "%#{query}%", "%#{query}%")
+        # where("title like ? OR body like ?", "%#{query}%", "%#{query}%")
+        joins(:action_text_rich_text).where("action_text_rich_texts.body LIKE ?", "%#{query}%")
     end
 end
